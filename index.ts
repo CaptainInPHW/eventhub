dfefddeddc2ecdddc8fae95d3d25d965591720b9
jiangtao.dai <jiangtao.dai@aihuishou.com>
2021-03-02 18:10:57 +0800

    refactor

--- index.ts
+++ index.ts
@@ -1,37 +1,78 @@
 interface Events {
-  [eventName: string]: Function[];
+  [eventName: string]: Array<(...args: unknown[]) => void>;
 }
 
 class EventHub {
   private events: Events = {};
 
-  public discribe(eventName: string, callback: () => void) {
+  /**
+   * Subscribe to the event.
+   * 
+   * @param eventName
+   * @param callback
+   * @returns {number} The id of the callback
+   */
+  public subscribe(eventName: string, callback: (...args: unknown[]) => void): number {
     if (!this.events[eventName]) {
       this.events[eventName] = [callback];
     }
-
     this.events[eventName].push(callback);
+    return this.events[eventName].length - 1;
   }
 
-  public undiscribe(eventName: string, callback: () => void) {
+  /**
+   * Subscribe to the event once, when the event is dispatched, the callback will be destroyed after execution.
+   * 
+   * @param eventName
+   * @param callback
+   */
+  public subscribeOnce(eventName: string, callback: (...args: unknown[]) => void): void {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    const wrapper = (...args: unknown[]) => {
+      callback(args);
+      // Don't use the splice method, otherwise the ids of the rest of the callbacks will be confused.
+      this.events[eventName][this.events[eventName].length] = undefined;
+    }
+    this.events[eventName].push(wrapper);
+  }
+
+  /**
+   * Unsubscribe form the event through the callback id.
+   * 
+   * @param eventName 
+   * @param token Callback id
+   */
+  public unsubscribe(eventName: string, token: number): void {
     if (!this.events[eventName]) {
       return;
     }
-
-    const index = this.events[eventName].indexOf(callback);
-
-    if (index > 0) {
-      this.events[eventName].splice(index, 1);
-    }
+    // Don't use the splice method, otherwise the ids of the rest of the callbacks will be confused.
+    this.events[eventName][token] = undefined;
   }
 
-  public dispatch(eventName: string, ...params: any[]) {
+  /**
+   * Dispatch an event.
+   * 
+   * @param eventName 
+   * @param params 
+   */
+  public dispatch(eventName: string, ...params: unknown[]): void {
     if (!this.events[eventName]) {
       throw new Error('Event' + eventName + 'is not subscribed.');
     }
-
     this.events[eventName].forEach(callback => {
       callback(params);
     });
+  }
+
+  /**
+   * Destory an event.
+   * 
+   * @param eventName 
+   */
+  public destroy(eventName: string): void {
+    this.events[eventName] = [];
   }
 }
